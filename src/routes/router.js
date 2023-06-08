d97c2937bd21321b583549073eb45e31edc371f4
kevin-tooles <kevinazul999@gmail.com>
2023-06-08 16:39:02 -0500

    comentariar login

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/login', async (req, res) => {
+/* router.post('/login', async (req, res) => {
     await authController.login(req, res);
 });
 
 router.post('/register', async (req, res) => {
     await authController.register(req, res);
-});
+}); */
 
 
 /**Enviar Correos */
